8c5c8fcff38ca76a20fbca8d0deee6daa81e8cd8
Biyas Basak <basakbiyas001@gmail.com>
2021-03-31 13:00:10 -0400

    an example with variable rps

---
+++ sample/variable-rps.js
@@ -0,0 +1,48 @@
+const loadtest = require("../lib/loadtest");
+const configuration = require('./configuration.json');
+
+function statusCallback(error, result, latency) {
+    console.log('Current latency %j, result %j, error %j', latency);
+    console.log('----');
+    console.log('Request elapsed milliseconds: ', result.requestElapsed);
+    console.log('Request index: ', result.requestIndex);
+    console.log('Request loadtest() instance index: ', result.instanceIndex);
+}
+
+rps = configuration.rps;
+rpsInterval = configuration.interval;
+
+index = 0;
+// start timer 
+start = Date.now();
+
+const options = {
+    url: 'https://www.google.com',
+    maxRequests: 2000,
+    // starting rps
+    requestsPerSecond: 0.5,
+    concurrency: 2,
+    /**
+     * GWU:Custom parameters
+     */
+    rpsInterval,
+    updateRPSCallback() {
+        console.log("callback called");
+        if (index < rps.length) {
+            return rps[index++];
+        }
+        // if the index >= than length of array select the last index
+        else {
+            return rps[rps.length - 1];
+        }
+    },
+    statusCallback: statusCallback,
+};
+
+loadtest.loadTest(options, function (error) {
+    if (error) {
+        return console.error('Got an error: %s', error);
+    }
+    console.log('Tests run successfully');
+});
+
